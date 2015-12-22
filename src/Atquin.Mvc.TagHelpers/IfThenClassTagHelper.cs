5151d517e527b51f5ee6933dea5eed3e6c1ea948
Stafford Williams <stafford.williams@gmail.com>
2015-12-22 15:04:45 +1100

    Initial commit

---
+++ src/Atquin.Mvc.TagHelpers/IfThenClassTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace Atquin.Mvc.TagHelpers
+{
+    [HtmlTargetElement("span", Attributes = IfAttributeName)]
+    [HtmlTargetElement("span", Attributes = ThenClassAttributeName)]
+    public class IfThenClassTagHelper : TagHelper
+    {
+        private const string IfAttributeName = "if";
+        private const string ThenClassAttributeName = "then-class";
+
+        [HtmlAttributeName(IfAttributeName)]
+        public bool If { get; set; }
+
+        [HtmlAttributeName(ThenClassAttributeName)]
+        public string ThenClass { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (If)
+                output.Attributes.Add("class", ThenClass);
+        }
+    }
+}
---
+++ src/Atquin.Mvc.TagHelpers/MenuAnchorTagHelper.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.TagHelpers;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+
+namespace Atquin.Mvc.TagHelpers
+{
+    [HtmlTargetElement("li", Attributes = TextAttributeName)]
+    [HtmlTargetElement("li", Attributes = ControllerAttributeName)]
+    [HtmlTargetElement("li", Attributes = ActionAttributeName)]
+    public class MenuAnchorTagHelper : TagHelper
+    {
+        private const string TextAttributeName = "text";
+        private const string ActionAttributeName = "action";
+        private const string ControllerAttributeName = "controller";
+        private const string BadgeAttributeName = "badge";
+
+        [HtmlAttributeName(TextAttributeName)]
+        public string Text { get; set; }
+
+        [HtmlAttributeName(ControllerAttributeName)]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName(ActionAttributeName)]
+        public string Action { get; set; }
+
+        [HtmlAttributeName(BadgeAttributeName)]
+        public int? Badge { get; set; }
+
+        protected IUrlHelper _urlHelper { get; set; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public MenuAnchorTagHelper(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string actionName = ViewContext.RouteData.Values["action"].ToString().ToLower();
+            string controllerName = ViewContext.RouteData.Values["controller"].ToString().ToLower();
+            if (Action.ToLower() == actionName && controllerName == Controller.ToLower())
+            {
+                output.Attributes.Add("class", "active");
+            }
+
+            var url = _urlHelper.Action(Action, Controller);
+
+            var text = Text;
+            if (Badge.HasValue)
+                text += $" <span class='badge'>{ Badge.Value }</span>";
+
+            output.Content.SetHtmlContent($"<a href='{url}'>{text}</a>");
+        }
+    }
+}
